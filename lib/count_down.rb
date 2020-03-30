e4a1fe3f175784eedcf9f1953cbc7639513bb1ac
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-30 20:29:58 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,8 +1,9 @@
 # Write your code here
 def countdown
   counter = 10
-while counter > 0 do
-  puts counter
-  counter -= 1
+  while counter > 0 do
+   puts counter
+   counter -= 1
+  end 
+ puts "Happy New Year!"
 end
-puts "Happy New Year!"
